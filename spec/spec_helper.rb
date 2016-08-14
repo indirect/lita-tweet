2a2212c4c98e6be77b688aaf44d0d4d21076a5c6
Andre Arko <andre@arko.net>
2016-08-13 19:06:56 -0700

    some rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ require "lita/rspec"
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
 Lita.version_3_compatibility_mode = false
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = ".rspec_examples"
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
