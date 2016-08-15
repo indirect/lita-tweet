191705fa5ef54f6eaabca03f1c32df59ac2065cb
Andre Arko <andre@arko.net>
2016-08-14 17:17:22 -0700

    add back coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
+require "coveralls"
 require "lita-tweet"
 require "lita/rspec"
 require "webmock/rspec"
+
+Coveralls.wear!
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
